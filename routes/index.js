bd26d737a5a0afb74b416cf958fe00bbc8f14582
nikmul19 <nikmul19@gmail.com>
2020-02-25 15:08:30 +0530

    Test Github Webhook

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,21 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
+router.post('/', function(req, res, next) {
+
+  console.log("hello");
+  console.log(req);
+
+
+  res.sendFile('/signup.html' , { root : "./public"})
+
+});
+
 router.get('/', function(req, res, next) {
 
   console.log("hello");
+
+  
 
 
   res.sendFile('/signup.html' , { root : "./public"})
